f801945c1ba88f7e833837d90b249a118b13c5b1
Jan Hendrik Lübke <35396725+akaSyntaax@users.noreply.github.com>
2022-02-22 15:55:09 +0000

    Fix error which caused the creation of invalid database tables

--- routes/connections.go
+++ routes/connections.go
@@ -19,10 +19,10 @@ func DBConnection() *sql.DB {
 }
 
 func InitializeDatabase() {
-	_, _, createErr := ExecuteNonQuery("CREATE TABLE IF NOT EXISTS Bookmarks (ID integer primary key not null, Title varchar(64) null, URL varchar(256) not null, Created timestamp default current_timestamp not null)")
+	_, _, createErr := ExecuteNonQuery("CREATE TABLE IF NOT EXISTS Bookmarks (ID integer primary key not null, Title varchar(64) null, URL varchar(256) not null, Created timestamp default current_timestamp not null, UserID integer not null references Users on update restrict on delete cascade)")
 
 	if createErr == nil {
-		_, _, createErr := ExecuteNonQuery("CREATE TABLE IF NOT EXISTS Users (ID integer primary key not null, Username varchar(32) not null, PasswordHash varchar(1024) not null, Created timestamp default current_timestamp not null, UserID integer not null references Users on update restrict on delete cascade)")
+		_, _, createErr := ExecuteNonQuery("CREATE TABLE IF NOT EXISTS Users (ID integer primary key not null, Username varchar(32) not null, PasswordHash varchar(1024) not null, Created timestamp default current_timestamp not null)")
 
 		if createErr == nil {
 			return
